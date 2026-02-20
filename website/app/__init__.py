919872affc044f87abee2445fc96523a25d95fa4
Marcos Reyes <astronautmarkus@gmail.com>
2026-02-20 16:47:59 -0300

    feat: add ProxyFix middleware to handle reverse proxy headers

--- website/app/__init__.py
+++ website/app/__init__.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_mail import Mail
 from app.config.config import Config
+from werkzeug.middleware.proxy_fix import ProxyFix
 
 db = SQLAlchemy()
 migrate = Migrate()
@@ -15,6 +16,8 @@ def create_app():
 	db.init_app(app)
 	migrate.init_app(app, db)
 	mail.init_app(app)
+
+	app.wsgi_app = ProxyFix(app.wsgi_app, x_for=1, x_proto=1)
 
 	from app.routes.home import home_bp
 	app.register_blueprint(home_bp)
