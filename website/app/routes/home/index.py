5d95759ad184b06260f9fee5afa1b59794141650
Marcos Reyes <astronautmarkus@gmail.com>
2026-02-24 21:02:11 -0300

    feat: update index page to display age and favorite game, add GitHub button

--- website/app/routes/home/index.py
+++ website/app/routes/home/index.py
@@ -1,7 +1,16 @@
 
 from flask import render_template
 from . import home_bp
+from datetime import datetime
 
 @home_bp.route('/')
 def index():
-    return render_template('index.html')
+
+    born_day = '2003-03-16'
+
+    current_year = datetime.now().year
+    born_year = datetime.strptime(born_day, '%Y-%m-%d').year
+
+    current_age = current_year - born_year
+
+    return render_template('index.html', age=current_age, born_day=born_day)
