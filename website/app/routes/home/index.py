1593013322a5efe126d23be78f24552597658ea3
Marcos Reyes <astronautmarkus@gmail.com>
2026-02-24 23:14:42 -0300

    feat: add random quotes to index page and update marquee display

--- website/app/routes/home/index.py
+++ website/app/routes/home/index.py
@@ -1,7 +1,7 @@
-
 from flask import render_template
 from . import home_bp
 from datetime import datetime
+import random
 
 @home_bp.route('/')
 def index():
@@ -13,4 +13,20 @@ def index():
 
     current_age = current_year - born_year
 
-    return render_template('index.html', age=current_age, born_day=born_day)
+    quotes = [
+        "IF THE CODE WORKS, DON'T TOUCH IT!! - 2003, CREATED BY A HUMAN, FOR HUMANS",
+        "HOLD ON, IF LOVE IS THE ANSWER, YOU'RE HOME.",
+        "I'M NOT A ROBOT, I'M A HUMAN BEING.",
+        "IN A WORLD OF BAD PEOPLE, THE GREATEST ACT OF REBELLION IS TO BE GOOD.",
+        "LIFE IS A JOURNEY, ENJOY THE RIDE.",
+        "HAPPINESS IS NOT A DESTINATION, IT'S A WAY OF TRAVELING.",
+        "LIFE IS TOO SHORT TO WORRY ABOUT SMALL THINGS.",
+        "TECHNOLOGY IS REPLACEABLE, PEOPLE ARE NOT.",
+        "BE HAPPY OR FIT INTO SOCIETY?",
+        "THE DESTINY OF GREAT SOULS IS TO BE CRITICIZED BY THE MEDIOCRE.",
+        "NO MATTER WHERE YOU GO, EVERYBODY'S CONNECTED."
+    ]
+
+    random_quote = random.choice(quotes)
+
+    return render_template('index.html', age=current_age, born_day=born_day, quote=random_quote)
