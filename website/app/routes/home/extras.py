a860ef0a0da0fa1ad72082c170c7a57f5d347fb8
Marcos Reyes <astronautmarkus@gmail.com>
2026-02-27 02:42:17 -0300

    feat: update YouTube playlists with new iframe sizes and randomize playlist selection

--- website/app/routes/home/extras.py
+++ website/app/routes/home/extras.py
@@ -1,29 +1,49 @@
 from flask import render_template
 from . import home_bp
+import random
 
 music_playlists = [
     {
-        "iframe": '<iframe width="1897" height="787" src="https://www.youtube.com/embed/jcgDIUvLL6c" title="Di Gi Charat- Party Night" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>'
+        "iframe": '<iframe width="420" height="236" src="https://www.youtube.com/embed/jcgDIUvLL6c" title="Di Gi Charat- Party Night" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>'
     },
     {
-        "iframe": '<iframe width="560" height="315" src="https://www.youtube.com/embed/V_D2NSENY0g?si=QcELEtJg9Frvn-CX" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>'
+        "iframe": '<iframe width="420" height="236" src="https://www.youtube.com/embed/V_D2NSENY0g?si=QcELEtJg9Frvn-CX" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>'
     },
     {
-        "iframe": '<iframe width="560" height="315" src="https://www.youtube.com/embed/6RAehoXcOjI?si=d77dGB8MiuYnksK5" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>'
+        "iframe": '<iframe width="420" height="236" src="https://www.youtube.com/embed/6RAehoXcOjI?si=d77dGB8MiuYnksK5" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>'
     },
     {
-        "iframe": '<iframe width="560" height="315" src="https://www.youtube.com/embed/tjlvmb8SGEs?si=eGcllafTKfx5kMif" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>'
+        "iframe": '<iframe width="420" height="236" src="https://www.youtube.com/embed/tjlvmb8SGEs?si=eGcllafTKfx5kMif" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>'
     },
     {
-        "iframe": '<iframe width="560" height="315" src="https://www.youtube.com/embed/UsXubuXq1lM?si=nREEgMWazXkGe52j" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>'
+        "iframe": '<iframe width="420" height="236" src="https://www.youtube.com/embed/UsXubuXq1lM?si=nREEgMWazXkGe52j" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>'
     },
     {
-        "iframe": '<iframe width="560" height="315" src="https://www.youtube.com/embed/DwTinTO0o9I?si=3xVEGZZQ9jqoTXkI" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>'
+        "iframe": '<iframe width="420" height="236" src="https://www.youtube.com/embed/DwTinTO0o9I?si=3xVEGZZQ9jqoTXkI" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>'
     },
     {
-        "iframe": '<iframe width="560" height="315" src="https://www.youtube.com/embed/wbQEA_nxLaE?si=_iljl2KbGGs2NY3R" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>'
+        "iframe": '<iframe width="420" height="236" src="https://www.youtube.com/embed/wbQEA_nxLaE?si=_iljl2KbGGs2NY3R" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>'
+    },
+    {
+        "iframe": '<iframe width="420" height="236" src="https://www.youtube.com/embed/zTm666EKYRs?si=vyyBTpb23kw2Z637" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>'
+    },
+    {
+        "iframe": '<iframe width="420" height="236" src="https://www.youtube.com/embed/Uoox9fpmDP0?si=75VuRgclHpBJOCAk" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>'
+    },
+    {
+        "iframe": '<iframe width="420" height="236" src="https://www.youtube.com/embed/hJBHMUGI7Nc?si=qnMIUev0L0ZhpYHN" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>'
+    },
+    {
+        "iframe": '<iframe width="420" height="236" src="https://www.youtube.com/embed/jmKRgqWGrWc?si=MkYBYf5-x1MtVmMn" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>'
+    },
+    {
+        "iframe": '<iframe width="420" height="236" src="https://www.youtube.com/embed/lCW6ueHZKe8?si=O2lSIDOxg_nrhO2P" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>'
     }
 ]
+
+def _get_random_playlists():
+    return random.sample(music_playlists, len(music_playlists))
+
 
 underground_videos = [
     {
@@ -33,8 +53,8 @@ underground_videos = [
 
 @home_bp.route('/extras')
 def extras():
-    return render_template('extras.html', playlists=music_playlists, underground_videos=underground_videos)
+    return render_template('extras.html', playlists=_get_random_playlists(), underground_videos=underground_videos)
 
 @home_bp.route('/es/extras')
 def extras_es():
-    return render_template('/es/extras.html', playlists=music_playlists, underground_videos=underground_videos)
+    return render_template('/es/extras.html', playlists=_get_random_playlists(), underground_videos=underground_videos)
